196372d9fa1ba1a3257fc21959af264ac411569a
Liz <isabel.valdez436@gmail.com>
2018-03-12 01:05:23 -0700

    modified

--- BrainWaves/app/src/main/java/com/bignerdranch/android/brainwaves/MainMenuActivity.java
+++ BrainWaves/app/src/main/java/com/bignerdranch/android/brainwaves/MainMenuActivity.java
@@ -1,12 +1,14 @@
 package com.bignerdranch.android.brainwaves;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class MainMenuActivity extends Activity {
     private Button mProfile;
     private Button mListOfGames;
     private Button mDaily;
@@ -15,8 +17,8 @@ public class MainMenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main_menu);
-
         // connecting Game button to list of game activity
         mListOfGames = (Button) findViewById(R.id.list_of_games_button);
         mListOfGames.setOnClickListener(new View.OnClickListener() {
--- BrainWaves/app/src/main/java/com/bignerdranch/android/brainwaves/MathGame1Activity.java
+++ BrainWaves/app/src/main/java/com/bignerdranch/android/brainwaves/MathGame1Activity.java
@@ -1,22 +1,22 @@
 package com.bignerdranch.android.brainwaves;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MathGame1Activity extends AppCompatActivity {
+    private EditText mUser;
     private Button mNextButton;
     private TextView mQuestionTextView;
-
-    private MathQuestions[] mproblemBank = new MathQuestions[]{
-            new MathQuestions(R.string.problem1,8),
-            new MathQuestions(R.string.problem2, 15),
-            new MathQuestions(R.string.problem3, 4),
-            new MathQuestions(R.string.problem4, 2),
-    };
+    private int mUserLevel = 10;
+    private int mNumberOfQuestions= 10;
+    private MathQuestionGenerator mProblemBank = new MathQuestionGenerator(mNumberOfQuestions, mUserLevel);
     private  int mCurrentIndex = 0;
+
 
         @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +25,28 @@ public class MathGame1Activity extends AppCompatActivity {
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         updateQuestion();
-        // get input and do stuff
-            //
+        mUser = (EditText) findViewById(R.id.userInput);
         mNextButton = (Button) findViewById(R.id.nextButton);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex +1) % mproblemBank.length;
-                updateQuestion();
+               // mUserAnswer = Integer.parseInt(mUser.getText().toString());
+                mCurrentIndex = (mCurrentIndex + 1);
+                if(mCurrentIndex < mNumberOfQuestions) {
+                    updateQuestion();
+                }else{
+                    Intent intent = new Intent(MathGame1Activity.this, EndOfGameActivity.class);
+                    startActivity(intent);
+                }
             }
         });
 
+
     }
     private void updateQuestion(){
-        int question = mproblemBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
+        mQuestionTextView.setText(mProblemBank.getQuestion(mCurrentIndex));
     }
+
+
 
 }

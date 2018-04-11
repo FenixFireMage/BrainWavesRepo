db53b792e13fce3b17af1fce28eaf74909c67549
ShaynG <shayn.garron759@myci.csuci.edu>
2018-04-10 23:09:25 -0700

    almost looks like a game now
    in Nick Stern we trust

--- Flash/app/src/main/java/com/example/shayng/flash/MainActivity.java
+++ Flash/app/src/main/java/com/example/shayng/flash/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.shayng.flash;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,33 +25,33 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        final TextView TextField = findViewById(R.id.textView);
-        TextView CurrScore = findViewById(R.id.score);
-        //CurrScore.setText("SCORE: " + 0);
+        final TextView TimerField = findViewById(R.id.textView);
+        final TextView CurrScore = findViewById(R.id.score);
+
         new CountDownTimer(30000, 1000) {
 
             public void onTick(long millisUntilFinished) {
                 int timeLeft = (int)millisUntilFinished / 1000;
-                TextField.setText("Seconds remaining: " + timeLeft);
+                TimerField.setText("Seconds remaining: " + timeLeft);
+                CurrScore.setText("SCORE:    " + score);
 
-
-               // if(!isRed)
-               // {
-                    //setActivityBackgroundColor(0xfff00000);
+                if(!isRed)
+                {
+                    setActivityBackgroundColor( Color.GREEN);
                      if(rand.nextBoolean())
                      {
                         isRed=true;
-                        setActivityBackgroundColor(0xffffffff);
+                         setActivityBackgroundColor(Color.RED);
                     }
                     else{
 
-                         setActivityBackgroundColor(0xfff00000);
-
-
+                         if(rand.nextBoolean()) {
+                             setActivityBackgroundColor(0xffffff00);
+                         }
                      }
 
 
-             //  }
+               }
 
 
 
@@ -59,7 +60,8 @@ public class MainActivity extends AppCompatActivity {
             }
 
             public void onFinish() {
-                TextField.setText("Game Over");
+                CurrScore.setText("FINAL SCORE:   " + score);
+                TimerField.setText("Game Over");
             }
 
         }.start();
@@ -68,17 +70,19 @@ public class MainActivity extends AppCompatActivity {
     public void onFlash(View view){
 
 
-       // CurrScore.setText("SCORE: " + 0);
-       // if(isRed)
-       // {
-           // isRed=false;
-        setActivityBackgroundColor(0xfff00000);
-           // score+=100;
-       // }
-       // else
-       // {
-           // score-=100;
-       // }
+
+        if(isRed)
+        {
+            isRed=false;
+            setActivityBackgroundColor( Color.GREEN);
+
+            score+=100;
+
+        }
+        else
+        {
+            score-=100;
+        }
 
 
 

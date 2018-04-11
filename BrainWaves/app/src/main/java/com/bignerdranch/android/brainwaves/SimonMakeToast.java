5a2247ab89b841f1d53ab8bc6350e3fe839d0489
Paul Banyai <pjbanyai@gmail.com>
2018-04-11 08:04:30 -0700

    Started to Add Simon Game

---
+++ BrainWaves/app/src/main/java/com/bignerdranch/android/brainwaves/SimonMakeToast.java
@@ -0,0 +1,24 @@
+package com.bignerdranch.android.brainwaves;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Suzanne on 3/8/2018.
+ * Holds a static 'toast" function that makes sure only one toast is displayed on screen
+ * at a time, preventing a long toast-queue buildup
+ */
+
+public class SimonMakeToast {
+    //makes a toast and cancels a previous toast if there was one.
+    private static Toast toast;
+    public static void toast(Context context, String text){
+        if (toast != null){
+            toast.cancel();
+        }
+
+        toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
+        toast.show();
+
+    }
+}

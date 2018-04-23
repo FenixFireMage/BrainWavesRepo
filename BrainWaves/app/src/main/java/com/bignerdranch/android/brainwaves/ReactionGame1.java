12595314694fa532ad897533ee22addac258711e
ShaynG <shayn.garron759@myci.csuci.edu>
2018-04-23 04:55:40 -0700

    added dumb click counter game

--- BrainWaves/app/src/main/java/com/bignerdranch/android/brainwaves/ReactionGame1.java
+++ BrainWaves/app/src/main/java/com/bignerdranch/android/brainwaves/ReactionGame1.java
@@ -1,27 +1,48 @@
 package com.bignerdranch.android.brainwaves;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.view.View;
 
+
 public class ReactionGame1 extends AppCompatActivity {
     int clickcount=0;
-
+    int timeLeft;
     TextView showValue;
-
+    TextView currTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reaction_game1);
 
+        currTime = findViewById(R.id.timeLeft);
+        showValue = findViewById(R.id.counter);
 
-        showValue = (TextView) findViewById(R.id.counter);
+        new CountDownTimer(15000, 100) {
+
+            public void onTick(long millisUntilFinished) {
+                timeLeft = (int)millisUntilFinished / 1000;
+                int stimeleft = (int)millisUntilFinished - (timeLeft*1000);
+                currTime.setText("Time Left:" + timeLeft + "." + stimeleft/100);
+
+            }
+
+            public void onFinish() {
+
+                currTime.setText("Game Over");
+            }
+
+        }.start();
     }
 
     public void counter(View view){
+        if(timeLeft!=0)
         clickcount++;
-        showValue.setText(Integer.toString(clickcount));
+
+        showValue.setText("Score:" + clickcount);
 
     }
 }

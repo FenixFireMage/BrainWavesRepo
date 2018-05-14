a9c88ca1a442965f0f0b458365034c98ea6d29c7
ShaynG <35824284+ShaynG@users.noreply.github.com>
2018-05-14 02:22:53 -0700

    made logic game logical

--- BrainWaves/app/src/main/java/com/bignerdranch/android/brainwaves/LogicGame1.java
+++ BrainWaves/app/src/main/java/com/bignerdranch/android/brainwaves/LogicGame1.java
@@ -8,23 +8,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.ImageView;
+import java.util.Random;
+import android.widget.TextView;
 public class LogicGame1 extends AppCompatActivity {
+
+    public int score = 0;
+    int level = 1;
+   //public enum answers{1,2,3,4};
+    public int rightAnswer=-1;
     private Button mOne;
     private Button mTwo;
     private Button mThree;
     private Button mFour;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logic_game1);
+
+        generateQuestion(1);
         mOne = (Button) findViewById(R.id.button);
         mOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Start ListOfGamesActivity
-                Intent intent = new Intent(LogicGame1.this, ListOfGamesActivity.class);
-                startActivity(intent);
+                generateQuestion(1);
             }
         });
         mTwo = (Button) findViewById(R.id.button3);
@@ -32,8 +42,7 @@ public class LogicGame1 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Start ListOfGamesActivity
-                Intent intent = new Intent(LogicGame1.this, ListOfGamesActivity.class);
-                startActivity(intent);
+                generateQuestion(2);
             }
         });
         mThree = (Button) findViewById(R.id.button4);
@@ -41,8 +50,7 @@ public class LogicGame1 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Start ListOfGamesActivity
-                Intent intent = new Intent(LogicGame1.this, ListOfGamesActivity.class);
-                startActivity(intent);
+                generateQuestion(3);
             }
         });
         mFour = (Button) findViewById(R.id.button5);
@@ -50,10 +58,38 @@ public class LogicGame1 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Start ListOfGamesActivity
-                Intent intent = new Intent(LogicGame1.this, MainMenuActivity.class);
-                startActivity(intent);
+                generateQuestion(4);
             }
         });
+    }
+
+    public void generateQuestion(int answer){
+
+
+        if(rightAnswer==answer){
+            score++;
+        }
+        level++;
+
+        final TextView CurrScore = findViewById(R.id.logicScore);
+        CurrScore.setText("Score:"+score);
+
+        ImageView shape = findViewById(R.id.shape);
+        //CurrScore.setText(rightAnswer + "Score:" + score);
+
+        Random r = new Random();
+        rightAnswer = r.nextInt(4)+1;
+
+        switch(rightAnswer) {
+            case 1: shape.setImageResource(R.drawable.shape3);
+            break;
+            case 2: shape.setImageResource(R.drawable.shape4);
+            break;
+            case 3: shape.setImageResource(R.drawable.shape5);
+            break;
+            case 4: shape.setImageResource(R.drawable.shape6);
+            break;
+        }
     }
 
 }
